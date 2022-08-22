82ec772feac51e46570ee82764dd5df707c7aac2
Harshal <harshalskahar389@gmail.com>
2022-08-22 21:17:19 +0530

    commit

--- C++/merge.cpp
+++ C++/merge.cpp
@@ -1,18 +1,56 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-
-
-void mergesort(){
-
-
+ 
+void merge(int* a,int *b,int l,int mid,int h)
+{
+    int i=l,k=l,j=mid+1;
+   
+    while(i<=mid && j<=h)
+    {
+        if(a[i]<=a[j])
+        {
+           b[k]=a[i];
+           i++;
+        }
+        else
+        {
+            b[k]=a[j];
+            j++;
+        }
+        k++;
+    }
+    while(j<=h)
+    {
+         b[k]=a[j];
+        j++;k++;
+    }
+    while(i<=mid)
+    {
+        b[k]=a[i];
+        i++;k++;
+    }
+    for(int i=l;i<=h;i++)
+    {
+        a[i]=b[i];
+    }
 }
-
+void mergesort(int a[],int l,int h)
+{
+    int b[h+1];
+    if(l<h){
+    int mid=(l+h)/2;
+    mergesort(a,l,mid);
+    mergesort(a,mid+1,h);
+    merge(a,b,l,mid,h);
+    }
+}
 int main(){
+
     int n;
     cout<<"Enter Size of array ";
     cin>>n;
     int a[n];
+
    
      clock_t start, end;
 
@@ -25,10 +63,10 @@ int main(){
     cout<<endl;
 
 
-    
+   
     start = clock();
 
-    mergesort(n,a);
+    mergesort(a,0,n-1);
      for(int i=0;i<n;i++)
      {
          cout<<a[i]<<" ";
@@ -38,12 +76,9 @@ int main(){
     end = clock();
 
 
-    cout<<endl;
-    cout<<endl;
-    cout<<endl;
-
-
-    
+   cout<<endl;
+   cout<<endl;
+   cout<<endl;
     double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
     cout << "Time taken by program is : " << fixed 
          << time_taken << setprecision(5);
@@ -51,4 +86,6 @@ int main(){
     
     
     return 0;
+    
+    
 }
